a459bf1b1760c0ffeabfdf76caec9f40dfddf28c
Peter Leconte <peter5leconte@gmail.com>
2022-08-19 13:26:22 +0200

    added other alrdy made games back in

--- src/commands.ts
+++ src/commands.ts
@@ -1,32 +1,54 @@
-import { ButtonInteraction, CommandInteraction, MessageActionRow, MessageButton, MessageEmbed, MessageSelectMenu, SelectMenuInteraction } from 'discord.js';
-import { DateTime } from 'luxon';
-import { Command, Response } from './models/command';
-import { Colors, EmbedBuilder } from './util/embed-builder';
-import { Buzzer, Coinflip, DiceRoll } from './games';
+import { CommandInteraction } from 'discord.js';
+import { Command } from './models/command';
+import { EmbedBuilder } from './util/embed-builder';
+import { Buzzer, Coinflip, DiceRoll, Challenge } from './games';
 
-export const defaultPrefix = '!';
-export const timeZones = ['Europe/Brussels', 'Australia/Melbourne', 'America/Detroit'];
-
-export const dateFormates = ['d/M/y h:m', 'd-M-y h:m'];
-
-const guildOnly: MessageEmbed = EmbedBuilder.error('This is a server only command.');
-
-//TODO check content of respond messages
 export const commands: Command[] = [
-  { // Buzzer
-    name: 'buzzer',
-    category: 'misc',
-    description: 'Create a buzzer for everyone',
-    options: [],
-    async response(interaction: CommandInteraction): Promise<void> {
-      if (!interaction.guild) {
-        interaction.reply({ embeds: [EmbedBuilder.error('This command can not be used outside of servers')] });
-        return;
-      }
-      if (!this.options) {
-        return;
-      }
-      new Buzzer(interaction).init();
-    }
-  },
+    { // Buzzer
+        name: 'buzzer',
+        category: 'misc',
+        description: 'Create a buzzer for everyone',
+        options: [],
+        async response(interaction: CommandInteraction): Promise<void> {
+            if (!interaction.guild) {
+                interaction.reply({ embeds: [EmbedBuilder.error('This command can not be used outside of servers')] });
+                return;
+            }
+            if (!this.options) {
+                return;
+            }
+            new Buzzer(interaction).init();
+        }
+    },
+    { // coinflip
+        name: 'coinflip',
+        category: 'misc',
+        description: 'Heads or tails?',
+        options: [{ required: false, type: 4, name: 'coinside', description: 'Which side of the coin you wanna bet for?', choices: [{ name: 'Heads', value: 1 }, { name: 'Tails', value: 0 }] }],
+        async response(interaction: CommandInteraction): Promise<void> {
+            if (!this.options) {
+                return;
+            }
+            interaction.reply({ embeds: [new Coinflip().flip(interaction.options.getInteger(this.options[0].name))] });
+        }
+    },
+    { // roll TODO make up of embed
+        name: 'roll',
+        category: 'misc',
+        description: 'Rolls a die or dice.',
+        options: [{ required: false, type: 4, name: 'faces', description: 'Amount of faces for your dice/die (default 6)' },
+        { required: false, type: 4, name: 'amount', description: 'Amount of dice/die (default 1)' }],
+        async response(interaction: CommandInteraction): Promise<void> {
+            if (!this.options)
+                return;
+            let times = interaction.options.getInteger(this.options[1].name);
+            let faces = interaction.options.getInteger(this.options[0].name);
+            if (!times)
+                times = 1;
+            if (!faces)
+                faces = 6;
+
+            interaction.reply({ embeds: [new DiceRoll().roll(times, faces)] });
+        }
+    },
 ];
--- src/games.ts
+++ src/games.ts
@@ -3,226 +3,226 @@ import { Colors } from './util/embed-builder';
 import { Logger } from './util/logger';
 
 export class Buzzer {
-  interaction: CommandInteraction
-  readonly expireTime = 60000 * 5;
-  usersBuzzed: GuildMember[] = [];
+    interaction: CommandInteraction
+    readonly expireTime = 60000 * 5;
+    usersBuzzed: GuildMember[] = [];
 
-  //Buzzer
-  readonly buttonBuzzer = [
-    new MessageButton({ label: 'Buzz', customId: 'buzz', style: 'DANGER', emoji: '🐝', disabled: true }),
-  ];
+    //Buzzer
+    readonly buttonBuzzer = [
+        new MessageButton({ label: 'Buzz', customId: 'buzz', style: 'DANGER', emoji: '🐝', disabled: true }),
+    ];
 
-  //Start Pauze Reset
-  readonly buttonsBuzzerControls = [
-    new MessageButton({ label: 'Start', customId: 'start', style: 'SUCCESS' }),
-    new MessageButton({ label: 'Pause', customId: 'pause', style: 'PRIMARY' }),
-    new MessageButton({ label: 'Reset', customId: 'reset', style: 'DANGER' }),
-  ];
+    //Start Pauze Reset
+    readonly buttonsBuzzerControls = [
+        new MessageButton({ label: 'Start', customId: 'start', style: 'SUCCESS' }),
+        new MessageButton({ label: 'Pause', customId: 'pause', style: 'PRIMARY' }),
+        new MessageButton({ label: 'Reset', customId: 'reset', style: 'DANGER' }),
+    ];
 
-  constructor(interaction: CommandInteraction) {
-    this.interaction = interaction;
-  }
-
-  init(): void {
-    this.interaction.reply({
-      embeds: [this.createEmbed(Colors.warning, 'Buzzer hasn\'t started yet')],
-      components: [new MessageActionRow({ components: this.buttonBuzzer }), new MessageActionRow({ components: this.buttonsBuzzerControls })]
-    });
-
-    const buzzFilter = (i: MessageComponentInteraction) => this.buttonBuzzer
-      .map(i => i.customId).includes(i.customId)
-      && i.message.interaction!.id == this.interaction.id;
-
-    const buzzerControlFilter = (i: MessageComponentInteraction) => this.buttonsBuzzerControls
-      .map(i => i.customId).includes(i.customId)
-      && i.user.id == this.interaction.user.id
-      && i.message.interaction!.id == this.interaction.id;
-
-    const buzzCollector = (<any>this.interaction.channel)?.createMessageComponentCollector({ filter: buzzFilter, time: this.expireTime });
-    const controlCollector = (<any>this.interaction.channel)?.createMessageComponentCollector({ filter: buzzerControlFilter, time: this.expireTime });
-
-    if (!buzzCollector) return;
-    if (!controlCollector) return;
-
-    // Collectors
-    buzzCollector.on('collect', (i: MessageComponentInteraction<CacheType>) => {
-      buzzCollector.resetTimer();
-      controlCollector.resetTimer();
-
-      if (!(i.member instanceof GuildMember)) return;
-
-      if (!this.usersBuzzed.includes(i.member)) {
-        this.usersBuzzed.push(i.member);
-      }
-
-      this.updateButtonInteraction(i, Colors.success, i.member.displayName + ' buzzed!');
-    });
-
-    controlCollector.on('collect', (i: MessageComponentInteraction<CacheType>) => {
-      buzzCollector.resetTimer();
-      controlCollector.resetTimer();
-
-      switch (i.customId) {
-        case this.buttonsBuzzerControls[0].customId: // start
-          this.buttonBuzzer[0].setDisabled(false);
-          this.updateButtonInteraction(i, Colors.success, 'Buzzer started!');
-          break;
-        case this.buttonsBuzzerControls[1].customId: // pauze
-          this.buttonBuzzer[0].setDisabled(true);
-          this.updateButtonInteraction(i, Colors.warning, 'Buzzer pauzed');
-          break;
-        case this.buttonsBuzzerControls[2].customId: // reset
-          this.buttonBuzzer[0].setDisabled(true);
-          this.usersBuzzed = []; // Empty users
-          this.updateButtonInteraction(i, Colors.warning, 'Buzzer reset');
-          break;
-      }
-    });
-
-    // End of collectors
-    controlCollector.on('end', () => this.endInteraction());
-  }
-
-  createEmbed(color: Colors, footer: string): MessageEmbed {
-    const userList: string[] = this.usersBuzzed.map((u, i) => `${++i}. ${u.displayName}`);
-    if (userList.length > 0) {
-      userList[0] = '**' + userList[0] + '**';
+    constructor(interaction: CommandInteraction) {
+        this.interaction = interaction;
     }
-    const embed = new MessageEmbed()
-      .setTitle('🅱uzzer')
-      .setDescription(userList.join('\n'))
-      .setFooter(footer)
-      .setColor(color as ColorResolvable);
-    return embed;
-  }
 
-  updateButtonInteraction(i: MessageComponentInteraction, color: Colors, footer: string): void {
-    i.update({
-      embeds: [this.createEmbed(color, footer)],
-      components: [new MessageActionRow({ components: this.buttonBuzzer }), new MessageActionRow({ components: this.buttonsBuzzerControls })]
-    });
-  }
+    init(): void {
+        this.interaction.reply({
+            embeds: [this.createEmbed(Colors.warning, 'Buzzer hasn\'t started yet')],
+            components: [new MessageActionRow({ components: this.buttonBuzzer }), new MessageActionRow({ components: this.buttonsBuzzerControls })]
+        });
 
-  async endInteraction(): Promise<void> {
-    this.interaction.editReply({
-      embeds: [
-        this.createEmbed(Colors.warning, 'Buzzer has timed out, you can always start a new one').setTitle('No more 🅱uzz 🐝')
-      ], components: []
-    }).catch(() => Logger.error('could not edit reply, message probably deleted games:l111'));
+        const buzzFilter = (i: MessageComponentInteraction) => this.buttonBuzzer
+            .map(i => i.customId).includes(i.customId)
+            && i.message.interaction!.id == this.interaction.id;
 
-  }
+        const buzzerControlFilter = (i: MessageComponentInteraction) => this.buttonsBuzzerControls
+            .map(i => i.customId).includes(i.customId)
+            && i.user.id == this.interaction.user.id
+            && i.message.interaction!.id == this.interaction.id;
+
+        const buzzCollector = (<any>this.interaction.channel)?.createMessageComponentCollector({ filter: buzzFilter, time: this.expireTime });
+        const controlCollector = (<any>this.interaction.channel)?.createMessageComponentCollector({ filter: buzzerControlFilter, time: this.expireTime });
+
+        if (!buzzCollector) return;
+        if (!controlCollector) return;
+
+        // Collectors
+        buzzCollector.on('collect', (i: MessageComponentInteraction<CacheType>) => {
+            buzzCollector.resetTimer();
+            controlCollector.resetTimer();
+
+            if (!(i.member instanceof GuildMember)) return;
+
+            if (!this.usersBuzzed.includes(i.member)) {
+                this.usersBuzzed.push(i.member);
+            }
+
+            this.updateButtonInteraction(i, Colors.success, i.member.displayName + ' buzzed!');
+        });
+
+        controlCollector.on('collect', (i: MessageComponentInteraction<CacheType>) => {
+            buzzCollector.resetTimer();
+            controlCollector.resetTimer();
+
+            switch (i.customId) {
+                case this.buttonsBuzzerControls[0].customId: // start
+                    this.buttonBuzzer[0].setDisabled(false);
+                    this.updateButtonInteraction(i, Colors.success, 'Buzzer started!');
+                    break;
+                case this.buttonsBuzzerControls[1].customId: // pauze
+                    this.buttonBuzzer[0].setDisabled(true);
+                    this.updateButtonInteraction(i, Colors.warning, 'Buzzer pauzed');
+                    break;
+                case this.buttonsBuzzerControls[2].customId: // reset
+                    this.buttonBuzzer[0].setDisabled(true);
+                    this.usersBuzzed = []; // Empty users
+                    this.updateButtonInteraction(i, Colors.warning, 'Buzzer reset');
+                    break;
+            }
+        });
+
+        // End of collectors
+        controlCollector.on('end', () => this.endInteraction());
+    }
+
+    createEmbed(color: Colors, footer: string): MessageEmbed {
+        const userList: string[] = this.usersBuzzed.map((u, i) => `${++i}. ${u.displayName}`);
+        if (userList.length > 0) {
+            userList[0] = '**' + userList[0] + '**';
+        }
+        const embed = new MessageEmbed()
+            .setTitle('🅱uzzer')
+            .setDescription(userList.join('\n'))
+            .setFooter(footer)
+            .setColor(color as ColorResolvable);
+        return embed;
+    }
+
+    updateButtonInteraction(i: MessageComponentInteraction, color: Colors, footer: string): void {
+        i.update({
+            embeds: [this.createEmbed(color, footer)],
+            components: [new MessageActionRow({ components: this.buttonBuzzer }), new MessageActionRow({ components: this.buttonsBuzzerControls })]
+        });
+    }
+
+    async endInteraction(): Promise<void> {
+        this.interaction.editReply({
+            embeds: [
+                this.createEmbed(Colors.warning, 'Buzzer has timed out, you can always start a new one').setTitle('No more 🅱uzz 🐝')
+            ], components: []
+        }).catch(() => Logger.error('could not edit reply, message probably deleted games:l111'));
+
+    }
 }
 
 // Unused but fully functional
 export class Challenge {
-  interaction: CommandInteraction
-  readonly challengeName: string
-  readonly expireTime = 60000
+    interaction: CommandInteraction
+    readonly challengeName: string
+    readonly expireTime = 60000
 
-  constructor(interaction: CommandInteraction, challengeName: string) {
-    this.challengeName = challengeName;
-    this.interaction = interaction;
-  }
+    constructor(interaction: CommandInteraction, challengeName: string) {
+        this.challengeName = challengeName;
+        this.interaction = interaction;
+    }
 
-  /**Will call callback when accepted */
-  init(callback: () => any): void {
-    const buttonsChallenge = [
-      new MessageButton({ label: 'Accept', customId: 'accept', style: 'SUCCESS' }),
-      new MessageButton({ label: 'Decline', customId: 'decline', style: 'DANGER' })
-    ];
-    const actionRowChallenge = new MessageActionRow({
-      components: buttonsChallenge
-    });
+    /**Will call callback when accepted */
+    init(callback: () => any): void {
+        const buttonsChallenge = [
+            new MessageButton({ label: 'Accept', customId: 'accept', style: 'SUCCESS' }),
+            new MessageButton({ label: 'Decline', customId: 'decline', style: 'DANGER' })
+        ];
+        const actionRowChallenge = new MessageActionRow({
+            components: buttonsChallenge
+        });
 
-    // Player being challenged may accept or decline
-    const player = this.interaction.options.getUser('player', true);
-    const filter = (i: MessageComponentInteraction) => buttonsChallenge
-      .map(i => i.customId).includes(i.customId)
-      && i.user.id == player.id
-      && i.message.interaction!.id == this.interaction.id;
-    const collector = (<any>this.interaction.channel)?.createMessageComponentCollector({ filter: filter, time: this.expireTime });
+        // Player being challenged may accept or decline
+        const player = this.interaction.options.getUser('player', true);
+        const filter = (i: MessageComponentInteraction) => buttonsChallenge
+            .map(i => i.customId).includes(i.customId)
+            && i.user.id == player.id
+            && i.message.interaction!.id == this.interaction.id;
+        const collector = (<any>this.interaction.channel)?.createMessageComponentCollector({ filter: filter, time: this.expireTime });
 
-    this.interaction.reply({
-      content: `<@!${player.id}>`,
-      embeds: [new MessageEmbed()
-        .setTitle(`${player.username} has been challenged by ${this.interaction.user.username}`)
-        .setDescription(`${this.interaction.user.username} has you challenged you for \`${this.challengeName}\`.\nWill you accept this challenge?`)
-        .setColor(Colors.success)
-      ], components: [actionRowChallenge]
-    });
+        this.interaction.reply({
+            content: `<@!${player.id}>`,
+            embeds: [new MessageEmbed()
+                .setTitle(`${player.username} has been challenged by ${this.interaction.user.username}`)
+                .setDescription(`${this.interaction.user.username} has you challenged you for \`${this.challengeName}\`.\nWill you accept this challenge?`)
+                .setColor(Colors.success)
+            ], components: [actionRowChallenge]
+        });
 
 
-    if (!collector) return;
-    collector.on('collect', (i: MessageComponentInteraction<CacheType>) => {
-      switch (i.customId) {
-        case buttonsChallenge[0].customId:
-          collector.stop('accept');
-          callback();
-          break;
-        case buttonsChallenge[1].customId:
-          collector.stop('decline');
-          break;
-      }
-    });
+        if (!collector) return;
+        collector.on('collect', (i: MessageComponentInteraction<CacheType>) => {
+            switch (i.customId) {
+                case buttonsChallenge[0].customId:
+                    collector.stop('accept');
+                    callback();
+                    break;
+                case buttonsChallenge[1].customId:
+                    collector.stop('decline');
+                    break;
+            }
+        });
 
-    collector.on('end', (collected: Collection<string, MessageComponentInteraction<CacheType>>, reason: string) => {
-      const embed = new MessageEmbed();
-      if (reason == 'accept') {
-        return;
-      }
-      if (reason == 'decline') {
-        embed
-          .setTitle(`${player.username} was too afraid to accept this challenge!`)
-          .setColor(Colors.error);
-        this.interaction.editReply({ embeds: [embed], components: [] })
-          .catch(() => Logger.error('could not edit reply, message probably deleted games:l111'));
-      }
-      else {
-        embed
-          .setTitle(`${player.username} was too scared or slow to respond to this challenge.`)
-          .setColor(Colors.warning);
-        this.interaction.editReply({
-          embeds: [embed], components: [
-            new MessageActionRow({ components: actionRowChallenge.components.map(i => i.setDisabled(true)) })
-          ]
-        }).catch(() => Logger.error('could not edit reply, message probably deleted games:l111'));
-      }
-    });
-  }
+        collector.on('end', (collected: Collection<string, MessageComponentInteraction<CacheType>>, reason: string) => {
+            const embed = new MessageEmbed();
+            if (reason == 'accept') {
+                return;
+            }
+            if (reason == 'decline') {
+                embed
+                    .setTitle(`${player.username} was too afraid to accept this challenge!`)
+                    .setColor(Colors.error);
+                this.interaction.editReply({ embeds: [embed], components: [] })
+                    .catch(() => Logger.error('could not edit reply, message probably deleted games:l111'));
+            }
+            else {
+                embed
+                    .setTitle(`${player.username} was too scared to respond to this challenge.`)
+                    .setColor(Colors.warning);
+                this.interaction.editReply({
+                    embeds: [embed], components: [
+                        new MessageActionRow({ components: actionRowChallenge.components.map(i => i.setDisabled(true)) })
+                    ]
+                }).catch(() => Logger.error('could not edit reply, message probably deleted games:l111'));
+            }
+        });
+    }
 }
 
 export class DiceRoll {
-  roll(amount: number, faces: number): MessageEmbed {
-    const embed = new MessageEmbed;
-    const dice = [];
-    for (let i = 0; i < amount; i++) {
-      dice.push(Math.floor(Math.random() * faces + 1));
-    }
+    roll(amount: number, faces: number): MessageEmbed {
+        const embed = new MessageEmbed;
+        const dice = [];
+        for (let i = 0; i < amount; i++) {
+            dice.push(Math.floor(Math.random() * faces + 1));
+        }
 
-    if (amount === 1) {
-      embed.setTitle(`:game_die: 1d${faces}`)
-        .setDescription(`You rolled a ${dice[0]}.`)
-        .setColor(Colors.error);
+        if (amount === 1) {
+            embed.setTitle(`:game_die: 1d${faces}`)
+                .setDescription(`You rolled a ${dice[0]}.`)
+                .setColor(Colors.error);
+        }
+        else {
+            embed.setTitle(`${amount}d${faces} :game_die:`)
+                .setDescription(`You rolled: ${dice.join(' + ')}\nTotal: ${dice.reduce((p, c) => p + c, 0)}`)
+                .setColor(Colors.error);
+        }
+        return embed;
     }
-    else {
-      embed.setTitle(`${amount}d${faces} :game_die:`)
-        .setDescription(`You rolled: ${dice.join(' + ')}\nTotal: ${dice.reduce((p, c) => p + c, 0)}`)
-        .setColor(Colors.error);
-    }
-    return embed;
-  }
 }
 
 export class Coinflip {
-  flip(side?: number | null): MessageEmbed {
-    const flip = Math.round(Math.random());
-    const embed = new MessageEmbed({
-      color: Colors.warning
-    }).setTitle(flip ? ':coin: Heads!' : ':coin: Tails!');
+    flip(side?: number | null): MessageEmbed {
+        const flip = Math.round(Math.random());
+        const embed = new MessageEmbed({
+            color: Colors.warning
+        }).setTitle(flip ? ':coin: Heads!' : ':coin: Tails!');
 
-    if (side != null)
-      flip == side ? (embed.setDescription('You\'ve won! :tada:')) : (embed.setDescription('You\'ve lost...'));
+        if (side != null)
+            flip == side ? (embed.setDescription('You\'ve won! :tada:')) : (embed.setDescription('You\'ve lost...'));
 
-    return embed;
-  }
+        return embed;
+    }
 }
